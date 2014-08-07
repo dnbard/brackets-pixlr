27215c9e7bb0ff1867a19e614973ee6b4a56b1ff
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2014-08-07 12:43:40 +0300

    initial commit

---
+++ services/modal.js
@@ -0,0 +1,20 @@
+define(function(require, exports){
+    var Dialogs = brackets.getModule('widgets/Dialogs'),
+        pixlrTemplate = require('text!../templates/modal.html'),
+        installFlashTemplate = require('text!../templates/installFlash.html');
+
+    function showHandler(){
+        if (isFlashPlayerInstalled()){
+            Dialogs.showModalDialogUsingTemplate(pixlrTemplate)._$dlg;
+        } else {
+            Dialogs.showModalDialogUsingTemplate(installFlashTemplate)._$dlg;
+        }
+    }
+
+    function isFlashPlayerInstalled(){
+        return false;
+        return !!navigator.mimeTypes ["application/x-shockwave-flash"];
+    }
+
+    exports.showHandler = showHandler;
+});
